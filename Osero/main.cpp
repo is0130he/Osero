3bf247216f5c7f84ed6ada578abd13645d9b964c
西村　宗高 <munetakanishimura@nishimurashuukounoMacBook-Pro.local>
2017-05-27 05:46:04 +0900

    Initial Commit

---
+++ Osero/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Osero
+//
+//  Created by 西村　宗高 on 2017/05/27.
+//  Copyright © 2017年 西村　宗高. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
